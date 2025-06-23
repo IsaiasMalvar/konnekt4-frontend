ac2b85fbed92d3ddc8265a2303860585ad49bb56
Isaias Malvar <isaiasmg93@hotmail.com>
2025-06-23 13:37:54 +0100

    Correct lobby room card text size

--- src/components/LobbyRoomCard.tsx
+++ src/components/LobbyRoomCard.tsx
@@ -11,10 +11,10 @@ const LobbyRoomCard = ({ id }: LobbyRoomCardProps): React.ReactElement => {
   };
   return (
     <div
-      className="cursor-pointer hover:scale-105 hover:z-50 backdrop-blur-xl h-[] bg-cyan-300/50  text-amber-300/80 rounded-3xl p-5 text-center flex flex-col justify-center items-center"
+      className="cursor-pointer hover:scale-105 hover:z-50 backdrop-blur-xl bg-cyan-300/50  text-amber-300/80 rounded-3xl p-5 text-center flex flex-col justify-center items-center"
       onClick={() => onClick(id)}
     >
-      <span className=" font-lucky text-3xl sm:text-4xl md:text-5xl lg:text-7xl p-2 uppercase">{`ROOM ${id}`}</span>
+      <span className=" font-lucky text-3xl sm:text-4xl md:text-5xl lg:text-6xl p-2 uppercase">{`ROOM ${id}`}</span>
       <span className="font-mono font-extrabold text-orange-700 text-xl sm:text-2xl uppercase">
         Available
       </span>
